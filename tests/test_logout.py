9dcab316a682351939854025f8b0039fe332a992
Lipatnikova <aerovaya@gmail.com>
2023-11-14 21:36:50 +0400

    feat: add test logout after login status code

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,6 +1,7 @@
 import logging
 
-from data.expected_result import ExpectedText
+from data.data import DataLoginUser
+from data.expected_result import ExpectedText, ExpectedRequestsResult
 from data.endpoints import UrlAndEndPoints as EndPoint
 from utils.http_handler import HTTPHandler
 
@@ -8,6 +9,15 @@ logger = logging.getLogger("api")
 
 
 class TestsLogoutEndPoint:
+
+    def test_logout_after_login_status_code(self):
+        response1 = HTTPHandler.post(f'{EndPoint.LOGIN}', DataLoginUser.data_login)
+        logger.info(response1.json())
+        response2 = HTTPHandler.post(f'{EndPoint.LOGOUT}', {})
+        response_status = response2.status_code
+        logger.info(response_status)
+        assert response_status == ExpectedRequestsResult.STATUS_CODE_OK, \
+            logger.warning('Response status code is incorrect')
 
     def test_logout_verify_content_type(self):
         response = HTTPHandler.get(f'{EndPoint.LOGOUT}')
