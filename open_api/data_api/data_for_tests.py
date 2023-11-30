d0805f6ea03cbf5858622135c9cec888a3394af7
Lipatnikova <aerovaya@gmail.com>
2023-11-30 09:11:07 +0400

    init: open_api

---
+++ open_api/data_api/data_for_tests.py
@@ -0,0 +1,15 @@
+# This section contains data for API tests
+class DataForTests:
+    POSTAL_CODE = '20852'
+    COUNTRY_CODE = 'US'
+    API_KEI = 'b6f6e0909f9015302989a3cffeec09f5'
+
+
+# This section contains the main code statuses
+class StatusCode:
+        STATUS_CODE_OK = 200
+
+
+# This section contains the expected text
+class ExpectedText:
+    content_type = 'application/json; charset=utf-8'
---
+++ open_api/data_api/data_urls.py
@@ -0,0 +1,7 @@
+"""
+This page contains urls
+"""
+
+
+class Urls:
+    BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
